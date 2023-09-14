edb99bfc3b4bf58bf4ba6a4013f75104bc050e12
Yuri Abe <57399611+aberi2021@users.noreply.github.com>
2023-09-15 00:06:29 +0900

    [delate]デプロイテストのためエラーが起きている中身をコメントアウト

--- src/pages/blogs/[id].tsx
+++ src/pages/blogs/[id].tsx
@@ -1,80 +1,80 @@
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
-import { client } from '@/lib/client'
-import Image from 'next/image'
-import Button from '@/components/atoms/button'
+// import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+// import { client } from '@/lib/client'
+// import Image from 'next/image'
+// import Button from '@/components/atoms/button'
 
-//型定義
-type Props = {
-  data: {
-    content: string
-    createsAt: string
-    eyecatch: {
-      url: string
-      height: number
-      width: number
-    }
-    id: string
-    publishedAt: string
-    revisedAt: string
-    title: string
-    updateAt: string
-  }
-}
+// //型定義
+// type Props = {
+//   data: {
+//     content: string
+//     createsAt: string
+//     eyecatch: {
+//       url: string
+//       height: number
+//       width: number
+//     }
+//     id: string
+//     publishedAt: string
+//     revisedAt: string
+//     title: string
+//     updateAt: string
+//   }
+// }
 
-const NewsDetail: NextPage<Props> = (props) => {
-  console.log(props)
+// const NewsDetail: NextPage<Props> = (props) => {
+//   console.log(props)
 
-  return (
-    <>
-      <h1>{props.data.title}</h1>
-      <Button href={'/'} label={'トップへ戻る'} />
-      <Image
-        src={props.data.eyecatch.url}
-        width={props.data.eyecatch.width}
-        height={props.data.eyecatch.height}
-        //数値を指定してもOK
-        // width={1000}
-        // height={500}
+//   return (
+//     <>
+//       <h1>{props.data.title}</h1>
+//       <Button href={'/'} label={'トップへ戻る'} />
+//       <Image
+//         src={props.data.eyecatch.url}
+//         width={props.data.eyecatch.width}
+//         height={props.data.eyecatch.height}
+//         //数値を指定してもOK
+//         // width={1000}
+//         // height={500}
 
-        //空の場合
-        alt={''}
-        //タイトルを入れる場合
-        //alt={props.data.title}
-      />
-      <div dangerouslySetInnerHTML={{ __html: props.data.content }}></div>
-    </>
-  )
-}
+//         //空の場合
+//         alt={''}
+//         //タイトルを入れる場合
+//         //alt={props.data.title}
+//       />
+//       <div dangerouslySetInnerHTML={{ __html: props.data.content }}></div>
+//     </>
+//   )
+// }
 
-export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
-  const res = await client.get({ endpoint: 'blogs', queries: { limit: 1000 } })
+// export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
+//   const res = await client.get({ endpoint: 'blogs', queries: { limit: 1000 } })
 
-  const paths = res.contents.map((post: { id: string }) => ({
-    params: { id: post.id },
-  }))
+//   const paths = res.contents.map((post: { id: string }) => ({
+//     params: { id: post.id },
+//   }))
 
-  return { paths, fallback: false }
-}
+//   return { paths, fallback: false }
+// }
 
-export const getStaticProps: GetStaticProps<Props, { id: string }> = async ({
-  params,
-}) => {
-  if (!params) {
-    return {
-      notFound: true,
-    }
-  }
+// export const getStaticProps: GetStaticProps<Props, { id: string }> = async ({
+//   params,
+// }) => {
+//   if (!params) {
+//     return {
+//       notFound: true,
+//     }
+//   }
 
-  const res = await client.get({
-    endpoint: 'blogs',
-    contentId: params.id as string,
-  })
+//   const res = await client.get({
+//     endpoint: 'blogs',
+//     contentId: params.id as string,
+//   })
 
-  return {
-    props: {
-      data: res,
-    },
-  }
-}
+//   return {
+//     props: {
+//       data: res,
+//     },
+//   }
+// }
 
-export default NewsDetail
+// export default NewsDetail
