864fe24dc3c1044b4ecf51f67e08586a85375461
Yuri Abe <57399611+aberi2021@users.noreply.github.com>
2023-09-29 14:04:33 +0900

    [udate]aboutの内容取得

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,36 +1,74 @@
-import { NextPageWithLayout } from 'next'
 import { GetStaticProps } from 'next'
+import { NextSeo } from 'next-seo'
+import styled from 'styled-components'
+import type { NextPageWithLayout } from '@/pages/_app'
 import DefaultLayout from '@/components/layout/default-layout'
 import { client } from '@/lib/client'
 
 type Props = {
-  about: {
+  aboutData: {
     context: string
+    title: string
   }
 }
 
-const About: NextPageWithLayout<Props> = ({ about }) => {
+const About: NextPageWithLayout<Props> = ({ aboutData }) => {
+  const { title, context } = aboutData
+
   return (
     <>
-      <h1>About</h1>
-      <div>{about.context}</div>
+      <NextSeo title={`${title} | あべのサイト`} />
+      <AboutPage>
+        <AboutTitle>{title}</AboutTitle>
+        <ContentsWrapper>
+          <div dangerouslySetInnerHTML={{ __html: context }}></div>
+        </ContentsWrapper>
+      </AboutPage>
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  // 全体記事新着6件を取得
-  const about = await client.get({
-    endpoint: 'about',
-  })
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const about = await client.get({ endpoint: 'about' })
 
   return {
     props: {
-      about: about.contents,
+      aboutData: about,
     },
   }
 }
 
 About.getLayout = (page) => <DefaultLayout>{page}</DefaultLayout>
+
+const AboutPage = styled.div`
+  margin-top: 1rem;
+  max-width: 47rem;
+  margin: 0 auto;
+`
+
+const AboutTitle = styled.h1`
+  font-size: 2rem;
+  margin-top: 2rem;
+`
+
+const ContentsWrapper = styled.div`
+  margin-top: 1rem;
+  pre,
+  code {
+    background-color: #000;
+    color: #fff;
+    font-size: 1rem;
+  }
+  pre {
+    padding: 1rem;
+    margin-top: 1rem;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+    overflow: auto;
+  }
+  p > code {
+    padding: 0 0.5rem;
+  }
+`
 
 export default About
